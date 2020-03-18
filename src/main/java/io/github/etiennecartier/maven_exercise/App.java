6e4187159205a9eedc719fded8cf07390d9eb325
EtienneCartier <et.cartier@gmail.com>
2020-03-18 08:54:10 +0100

    first commit

---
+++ src/main/java/io/github/etiennecartier/maven_exercise/App.java
@@ -0,0 +1,13 @@
+package io.github.etiennecartier.maven_exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
